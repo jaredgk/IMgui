a2b4d20b6a07002f4fc13155e70b698e10d7cad4
Jared Knoblauch <jaredknoblauch@gmail.com>
2016-05-26 14:08:49 -0400

    modified install script to check for mpicxx and compile single-thread version if not present

--- install_ima.sh
+++ install_ima.sh
@@ -1,2 +1,6 @@
 cd IMa
-mpicxx -o IMa2 *.cpp -DMPI_ENABLED -DXML_ENABLED
+if hash mpicxx 2>/dev/null; then
+	mpicxx -o IMa2 *.cpp -DMPI_ENABLED -DXML_ENABLED
+else
+	g++ -o IMa2 *.cpp -O2 -DXML_ENABLED
+fi
